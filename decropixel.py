ff5dcf4e7835a11e85fd542641bf479689008305
decrovert <decrovertp@gmail.com>
2022-07-18 19:56:45 +0100

    neural network

--- decropixel.py
+++ decropixel.py
@@ -2,8 +2,10 @@ if not __name__ == "__main__":
     exit("Don't run Decropixel as a library!")
 
 import sys
+import random; random.seed()
+import typing
 
-ARGV_LENGTH = len(sys.argv)
+ARGV_LENGTH: typing.Sized = len(sys.argv)
 
 if ARGV_LENGTH == 1:
     exit("Use the \'help\' option for help!")
@@ -12,20 +14,119 @@ elif ARGV_LENGTH > 2:
 
 del ARGV_LENGTH
 
-DATA_FILE_NAME = "data.txt"
-LICENSE_FILE_NAME = "LICENSE"
+DATA_FILE_NAME: str = "data.txt"
+LICENSE_FILE_NAME: str = "LICENSE"
+
+IMAGE_WIDTH: int = 8
+IMAGE_HEIGHT: int = 8
+IMAGE_SIZE: int = IMAGE_WIDTH * IMAGE_HEIGHT
+
+NETWORK_SIZE: typing.Sized = 5
+
+class Neuron:
+    def __init__(self, stimulation: float, bias: float):
+        self.stimulation = stimulation
+        self.bias = bias
+
+class NeuralNetwork:
+    def __init__(self):
+        self.layers = []
+        self.connections = []
+
+        for i in range(NETWORK_SIZE):
+            self.layers.append([])
+            
+            for _ in range(IMAGE_SIZE):
+                self.layers[i].append(Neuron(0.0, random.random() * 2 - 1))
+
+        for i in range((len(self.layers) - 1) * (IMAGE_SIZE ** 2)):
+            self.connections.append(random.random() * 2 - 1)
+    
+    def populate_input_neurons(self) -> None:
+        for i in range(len(self.layers[0])):
+            self.layers[0][i].stimulation = random.random() * 2 - 1
+    
+    def produce_output_image(self) -> None:
+        layer: typing.Sized = 1
+        connection_index: typing.Sized = 0
+
+        while layer < len(self.layers):
+            for neuron in range(len(self.layers[layer])):
+                neuron_stimulation: float = self.layers[layer - 1][neuron].stimulation
+
+                for _ in range(IMAGE_SIZE):
+                    neuron_stimulation += self.connections[connection_index]
+                    connection_index += 1
+                
+                neuron_stimulation += self.layers[layer][neuron].bias
+                self.layers[layer][neuron].stimulation = neuron_stimulation
+
+            layer += 1
+    
+    def get_output_image(self) -> typing.List[bool]:
+        output_image: typing.List[bool] = []
+
+        for neuron in self.layers[len(self.layers) - 1]:
+            output_image.append(neuron.stimulation > 0)
+        
+        return output_image
+
+    def draw_image(self) -> typing.List[bool]:
+        self.populate_input_neurons()
+        self.produce_output_image()
+        return self.get_output_image()
+
+neural_network = NeuralNetwork()
+
+def format_AI_image(image: typing.List[bool]) -> str:
+    formatted_image: str = ""
+
+    for _ in range(IMAGE_WIDTH + 2):
+        formatted_image += "-"
+
+    formatted_image += "\n"
+
+    for y in range(IMAGE_HEIGHT):
+        for x in range(IMAGE_WIDTH):
+            if x == 0:
+                formatted_image += "|"
+
+            match image[y * IMAGE_WIDTH + x]:
+                case True:
+                    formatted_image += "#"
+                case False:
+                    formatted_image += " "
+        
+        formatted_image += "|\n"
+    
+    for _ in range(IMAGE_WIDTH + 2):
+        formatted_image += "-"
+
+    return formatted_image
+
+def train_AI() -> None:
+    print("Evaluate the AI's creations to train it.")
+
+    draw_another = True
+
+    while draw_another:
+        print(format_AI_image(neural_network.draw_image()))
+
+        cool = input("Was this cool? [y]es/[n]o: ")
+
+        if not input("Do you wish to continue training the AI? [y]es/[n]o: ") == "y":
+            draw_another = False
 
 def reset_AI_data() -> None:
     print("This command will reset all the AI training data!")
-    confirmation = input("Are you sure you wish to continue? [y]es/[n]o: ")
 
-    if not confirmation == "y":
+    if not input("Are you sure you wish to continue? [y]es/[n]o: ") == "y":
         return
 
     print("Resetting all the data...")
 
     try:
-        with open(DATA_FILE_NAME, "w") as data_file:
+        with open(DATA_FILE_NAME, "w"):
             pass
     except:
         exit("Unable to open the data file to reset it!")
@@ -54,7 +155,7 @@ match sys.argv[1]:
         print("\tShow license information (if available).")
 
     case "train":
-        print("Train AI")
+        train_AI()
     
     case "reset":
         reset_AI_data()
